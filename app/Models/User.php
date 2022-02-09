5bdcf47a3502591d28d8b3c6f3546ac2f2b6778a
balda38 <alduin2012@gmail.com>
2022-02-09 23:17:20 +0300

    Fix user model for auth. Fix some moments in graphql schema.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+
+use Joselfonseca\LighthouseGraphQLPassport\HasLoggedInTokens;
+use Joselfonseca\LighthouseGraphQLPassport\MustVerifyEmailGraphQL;
+use Laravel\Passport\HasApiTokens;
 
 /**
  * @property int    $id
@@ -21,7 +24,7 @@ use Laravel\Sanctum\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasLoggedInTokens, MustVerifyEmailGraphQL;
 
     /**
      * The attributes that are mass assignable.
